df42bfe9c932ac02ec6716ffe248a724b33a77cd
Armin Javanshir <armin.javanshir@ericsson.com>
2025-03-20 14:40:07 +0100

    Updates.

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Home assistant configuration files.
 Follow these instructions for installing on Raspberry Pi:
 https://www.home-assistant.io/installation/raspberrypi
 
-Here is one screenshot taken from the live flooplan:
+Here is a screenshot taken from the live flooplan:
 
 ![Screenshot](www/screenshot.png)
--- python_scripts/msg_test.py
+++ python_scripts/msg_test.py
@@ -1,4 +1,4 @@
 # `data` is available as builtin and is a dictionary with the input data.
-name = data.get("name", "world")
+name = data.get("name", "Hoppsan")
 # `logger` and `time` are available as builtin without the need of explicit import.
-logger.info("Hello {} at {}".format(name, time.time()))
+logger.info("Data {} at {}".format(name, time.time()))
